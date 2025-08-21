249a6a9defd85ecbd0e1ad6a6ef490edc377fc9d
Manoj Mohan <haimanojkumar@gmail.com>
2025-08-21 15:16:30 +0530

    remove duplication

--- checkerlib/checker.cs
+++ checkerlib/checker.cs
@@ -4,42 +4,33 @@ using System.Diagnostics;
 
 public class Checker
 {
+    private static void displayVitalsAlert(string message)
+    {
+        Console.WriteLine(message);
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write("\r* ");
+            System.Threading.Thread.Sleep(1000);
+            Console.Write("\r *");
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+
     public static bool VitalsOk(float temperature, int pulseRate, int spo2)
     {
         if(temperature >102 || temperature < 95)
         {
-            Console.WriteLine("Temperature critical!");
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write("\r* ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write("\r *");
-                System.Threading.Thread.Sleep(1000);
-            }
+            displayVitalsAlert("Temperature critical!");
             return false;
         }
         else if (pulseRate < 60 || pulseRate > 100)
         {
-            Console.WriteLine("Pulse Rate is out of range!");
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write("\r* ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write("\r *");
-                System.Threading.Thread.Sleep(1000);
-            }
+            displayVitalsAlert("Pulse Rate is out of range!");
             return false;
         }
         else if (spo2 < 90)
         {
-            Console.WriteLine("Oxygen Saturation out of range!");
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write("\r* ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write("\r *");
-                System.Threading.Thread.Sleep(1000);
-            }
+            displayVitalsAlert("Oxygen Saturation is below normal!");
             return false;
         }
         Console.WriteLine("Vitals received within normal range");
